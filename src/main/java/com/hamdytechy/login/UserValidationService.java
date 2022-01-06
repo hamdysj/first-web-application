b4e8cd229edcaff910d8f5d5793568350e437624
Hamdallah Sulaimon <hamdysj@gmail.com>
2022-01-06 15:52:31 +0100

    update

--- src/main/java/com/hamdytechy/login/UserValidationService.java
+++ src/main/java/com/hamdytechy/login/UserValidationService.java
@@ -1,7 +1,10 @@
 package com.hamdytechy.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserValidationService {
-	
+
 	public boolean isUserValid(String user, String password)
 	{
 		return user.equalsIgnoreCase("Hamdy") 
